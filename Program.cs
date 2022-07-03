77a75bd746294e2a1f6802ead89ac04e9753c106
irinakuheika <irina.kugeiko469i@gmail.com>
2022-07-03 11:47:52 +0300

    add completed Task 2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB){
+    Console.WriteLine("Число А (" + numberA + ") больше числа В (" + numberB + ")");
+}
+else {
+    Console.WriteLine("Число B (" + numberB + ") больше числа A (" + numberA + ")");
+}
